1c6538f44181cb56a5d8b63314c1e05aa3785bf4
fmerkle <florian.merkle@mossglocal.com>
2020-12-12 13:38:32 +0000

    add proportion attribute to experience tile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import * as React from "react"
+import experiences from "../../content/en_experiences.json"
+
 
 // styles
 const pageStyles = {
@@ -10,6 +12,16 @@ const pageStyles = {
 }
 
 // markup
+const Tile = props => {
+  const {title, flex, visual, altText, link} = props
+  return (
+    <div style={{backgroundColor: color,  display: flex}}>
+      <a href={link}>
+        <img src={visual} alt={altText} style={{height:'100%', width:'100%', objectFit:'cover'}}/>
+      </a>
+    </div>
+  )
+}
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
@@ -21,9 +33,11 @@ const IndexPage = () => {
       </div>
       <div style={{width:'100%', height:'100vh', backgroundColor:'darkgrey', color:'white', display:'flex', flexDirection:'column'}}>
         <div style={{flex:1, flexDirection:'row', display:'flex'}}>
-          <div style={{backgroundColor:'green', flex:2}}></div>
-          <div style={{backgroundColor:'blue', flex:3}}></div>
-          <div style={{backgroundColor:'yellow', flex:3}}></div>
+        {experiences.experience.slice(0,3).map(exp => <Tile experience = {exp}/>)}
+          <Tile visual={experiences.experience[0].visual} altText={experiences.experience[0].altText} link={experiences.experience[0].link} color='green' flex={2} />
+          <Tile visual={experiences.experience[0].visual} altText={experiences.experience[0].altText} color='green' flex={3} />
+          <Tile visual={experiences.experience[0].visual} altText={experiences.experience[0].altText} color='green' flex={3} />
+          
           
 
           
