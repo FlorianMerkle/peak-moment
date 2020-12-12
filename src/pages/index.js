4309961b8c967e1fc3156e09afee339f249f7088
fmerkle <florian.merkle@mossglocal.com>
2020-12-12 14:15:43 +0000

    add Header logo and claim

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import experiences from "../../content/en_experiences.json"
+import generalSettings from "../../content/general_settings.json"
 import Tile from "../components/Tile"
 
 
@@ -15,13 +16,13 @@ const pageStyles = {
 // markup
 
 const IndexPage = () => {
+  const {title, logo} = generalSettings
   return (
     <main style={pageStyles}>
       <title>PEAK Moment</title>
-      <div style={{width:'100%', height:'200px', backgroundColor:'black', color:'white'}}>
-        <div style={{display:'flex'}}>
-          <img></img>
-        </div>        
+      <div style={{width:'100%', height:'200px', backgroundColor:'black', color:'white', display:'flex', }}>
+          <img src={logo} alt={title} style={{flex:'2',height:'100%', width:'100%', objectFit:'cover'}}/>
+          <div style={{display:'flex', justifyContent:'center', alignItems:'center', fontSize:'35px', flex:'5'}}>{title}</div>
       </div>
       <div style={{width:'100%', height:'100vh', backgroundColor:'darkgrey', color:'white', display:'flex', flexDirection:'column'}}>
         <div style={{flex:1, flexDirection:'row', display:'flex'}}>
