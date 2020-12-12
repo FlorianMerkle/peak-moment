5cf585bfc2037087488b6085670bfe1c162781c8
fmerkle <florian.merkle@mossglocal.com>
2020-12-12 18:25:54 +0000

    adjust styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,24 +70,24 @@ const IndexPage = () => {
       <div
         style={{
           width: "100%",
-          height: "100vh",
+          height: "99vh",
           backgroundColor: "darkgrey",
           color: "white",
           display: "flex",
           flexDirection: "column",
         }}
       >
-        <div style={{ flex: 1, flexDirection: "row", display: "flex" }}>
+        <div style={{ height: '33vh', flexDirection: "row", display: "flex" }}>
           {experiences.experience.slice(0, 3).map((exp) => (
             <Tile experience={exp} />
           ))}
         </div>
-        <div style={{ flex: 1, flexDirection: "row", display: "flex" }}>
+        <div style={{ height: '33vh', flexDirection: "row", display: "flex" }}>
           {experiences.experience.slice(3, 6).map((exp) => (
             <Tile experience={exp} />
           ))}
         </div>
-        <div style={{ flex: 1, flexDirection: "row", display: "flex" }}>
+        <div style={{ height: '33vh', flexDirection: "row", display: "flex" }}>
           {experiences.experience.slice(6, 9).map((exp) => (
             <Tile experience={exp} />
           ))}
