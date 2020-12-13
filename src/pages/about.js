4708c0e0aec041870661ed72f707cf29b2a3fab8
fmerkle <florian.merkle@mossglocal.com>
2020-12-13 08:29:56 +0000

    create about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,6 @@
 import * as React from "react";
-import { Link } from "gatsby"
-
-
-
-import generalSettings from "../../content/general_settings.json";
+import Header from "../components/Header";
+import Footer from "../components/Footer"
 
 // styles
 const pageStyles = {
@@ -17,69 +14,13 @@ const pageStyles = {
 // markup
 
 const AboutPage = () => {
-  const { title, logo } = generalSettings;
+
   return (
     <main style={pageStyles}>
       <title>PEAK Moment</title>
-      <div
-        style={{
-          width: "100%",
-          height: "200px",
-          backgroundColor: "black",
-          color: "white",
-          display: "flex",
-          padding: "50px",
-        }}
-      >
-        <div style={{ flex: 1, paddingLeft: "50px" }}>
-          <img
-            src={logo}
-            alt={title}
-            style={{ flex: "2", height: "100%", objectFit: "scale-down" }}
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              fontSize: "35px",
-              flex: "5",
-            }}
-          >
-            {title}
-          </div>
-        </div>
-        <div style={{ flex: 2 }}>
-          <div style={{ flex: 1 }}>
-            <Link to="/about">About</Link>
-          </div>
-          <div style={{ flex: 1 }}>
-            
-          </div>
-        </div>
-      </div>
-      <div
-        style={{
-          width: "100%",
-          height: "100vh",
-          backgroundColor: "darkgrey",
-          color: "white",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        
-      </div>
-      <div
-        style={{
-          width: "100%",
-          height: "300px",
-          backgroundColor: "black",
-          color: "white",
-        }}
-      >
-        Footer
-      </div>
+      <Header />
+      <div></div>
+      <Footer />
     </main>
   );
 };
