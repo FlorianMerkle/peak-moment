5ab7a6b0ad5a4d944355d721f3ecc9fba7138a26
fmerkle <florian.merkle@mossglocal.com>
2020-12-13 11:41:14 +0000

    remove image dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,6 @@ module.exports = {
     
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
+    
   ],
 };
