e0182363fff78f3e34d6deb980ed6b8bc27a7970
fmerkle <florian.merkle@mossglocal.com>
2020-12-13 18:50:04 +0000

    make experiences open in new tab

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -4,7 +4,7 @@ const Tile = props => {
     const {title, flex, visual, altText, link} = props.experience
     return (
       <div style={{ display: flex, position:'relative', cursor:'pointer', flex:flex}}>
-        <a href={link}>
+        <a href={link} target="_blank" >
           <img src={visual} alt={altText} style={{height:'100%', width:'100%', objectFit:'cover'}}/>
           <div style={{width:'100%', height:'55px', color:'white', fontSize:'25px', position:'absolute', bottom:'0px', left:'0px', backgroundColor:'#00000090'}}>
           <div style={{paddingLeft:'15px', paddingTop:'15px'}}>{title}</div>
