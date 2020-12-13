f3e326d6bfed2b4127dc046b4c3ffc4d1c6c64ea
fmerkle <florian.merkle@mossglocal.com>
2020-12-13 13:35:58 +0000

    configure mc plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-mailchimp',
       options: {
-          endpoint: '', // string; add your MC list endpoint here; see instructions below
+          endpoint: 'https://gmx.us7.list-manage.com/subscribe/post?u=7dfb256ed076b925767218b11&amp;id=a9d2c1e4ac', // string; add your MC list endpoint here; see instructions below
           timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
   },
