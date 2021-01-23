2c4b30eb29cdbec45f53df84dce0038f50977a39
Florian Merkle <merklefl@icloud.com>
2021-01-23 15:51:26 +0100

    add landingpage

---
+++ src/pages/landing.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import ReactMarkdown from "react-markdown";
+import { inject, observer } from "mobx-react";
+
+import Layout from "../components/Layout";
+import Newsletter from "../components/Newsletter";
+import ContactForm from "../components/ContactForm";
+import generalData from "../../content/general_settings.json";
+import aboutData from "../../content/de_about.json";
+
+
+const AboutPage = props => {
+  return(
+  <div>
+    {props.store.device==='desktop' && <Desktop/>}
+    {props.store.device!=='desktop' && <Mobile/>}
+    </div>)
+};
+
+const Desktop = () => {
+  return (
+    
+      <div>
+        
+      </div>
+    
+  );
+};
+
+const Mobile = () => {
+  return (
+    
+      <div>
+      </div>
+    
+  );
+};
+
+export default inject("store")(observer(LandingPage));
